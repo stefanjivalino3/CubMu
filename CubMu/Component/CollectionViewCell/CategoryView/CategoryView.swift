5b46c5021c7944586b04f093b2762d5a18a5bc3d
Stefan <stefanjivalino@gmail.com>
2023-08-12 14:35:03 +0700

    - Add Mulish font to project - Create category view cell

---
+++ CubMu/Component/CollectionViewCell/CategoryView/CategoryView.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryView.swift
+//  CubMu
+//
+//  Created by Stefan Jivalino on 12/08/23.
+//
+
+import UIKit
+
+class CategoryView: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
